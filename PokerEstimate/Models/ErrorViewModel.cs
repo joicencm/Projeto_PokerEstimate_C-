f44f82cd2a8784d1a99fb62afe2e24dda7bf1441
joicencm <joicencm@gmail.com>
2025-01-01 18:35:15 -0300

    Projeto Inicial

---
+++ PokerEstimate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokerEstimate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
