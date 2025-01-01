669ad2c9214e6aac77fdee37ffa0d57a29d68a78
joicencm <joicencm@gmail.com>
2025-01-01 20:12:10 -0300

    Soluções

--- PokerEstimate/obj/Release/net9.0/PokerEstimate.AssemblyInfo.cs
+++ PokerEstimate/obj/Release/net9.0/PokerEstimate.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokerEstimate")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44f82cd2a8784d1a99fb62afe2e24dda7bf1441")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokerEstimate")]
